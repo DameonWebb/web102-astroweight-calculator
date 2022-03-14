0b11f7e63415511100c5b4e8db8700b2f00b0ed6
Dameon Webb <dameonwebb@Dameons-Air-2.attlocal.net>
2022-03-14 13:22:22 -0700

    all tests passed

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
 // Write your JavaScript code here!
+var select = document.getElementById("planets");
+var planets = [ 
+    ['Pluto', 0.06], 
+    ['Neptune', 1.148], 
+    ['Uranus', 0.917], 
+    ['Saturn', 1.139], 
+    ['Jupiter', 2.640], 
+    ['Mars', 0.3895], 
+    ['Moon', 0.1655], 
+    ['Earth', 1], 
+    ['Venus', 0.9032], 
+    ['Mercury', 0.377], 
+    ['Sun', 27.9] 
+];
+
+for(var i = 0; i < planets.length; i++) {
+    var opt = planets[i][0];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+}
+function rsg(planetName) {
+    for(var i = 0; i < planets.length; i++) {
+        if(planetName == planets[i][0]) {
+            return planets[i][1];
+        } 
+        
+    } 
+}
+
+function calculateWeight(weight, planetName) {
+    return weight * rsg(planetName)  
+}
+
+function handleClick(e){
+    var weight = Number (document.getElementById("user-weight").value);
+    var planetSelected = document.getElementById("planets").value;
+    var newWeight =  calculateWeight(weight, planetSelected);
+    document.getElementById("output").textContent = "If you were on " + planetSelected  + ", you would weigh " + newWeight + "lbs!";
+
+}
+
+planets.forEach(element => console.log(element));
+
